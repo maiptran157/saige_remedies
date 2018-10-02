8e40d3330ef62c161653166a06818164dc4bed17
Mia Tran <maiptran157@gmail.com>
2018-10-02 09:59:57 -0700

    added remedy detail page wireframe and ailment container wireframe

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,15 +3,16 @@ import Header from './header';
 import ConditionContainer from './condition_container';
 import ConditionDetailContainer from './condition_detail_container';
 import RemedyDetailContainer from './remedy_detail_container';
+import { Route } from 'react-router-dom';
 
 import './app.css';
 
 const App = () => (
     <div className="container">
         <Header />
-        {/* <ConditionContainer/> */}
-        {/* <ConditionDetailContainer /> */}
-        <RemedyDetailContainer />
+        <Route exact path="/" component={ConditionContainer} />
+        {/* <Route path="/condition/:itemId" component={ConditionDetailContainer} />
+        <RemedyDetailContainer /> */}
     </div>
 );
 
