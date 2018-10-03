1cbcc5c1b36727e581051115241913b11a5da46d
Mia Tran <maiptran157@gmail.com>
2018-10-03 11:15:45 -0700

    work in progress. do not push

--- client/src/components/remedy_name_container.js
+++ client/src/components/remedy_name_container.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './condition_detail_container.css';
 
 export default props => {
+    // console.log(props);
+    const { remedyName } = props;
     return (
         <div className="remedy-name-container">
-            <span className="remedy-name">Lorem Ipsum</span>
+            <span className="remedy-name">{remedyName}</span>
         </div>
     )
 }
--- client/src/components/remedy_result_container.js
+++ client/src/components/remedy_result_container.js
@@ -4,16 +4,22 @@ import RemedyNameContainer from './remedy_name_container';
 import EndingMessage from './end_of_result_message';
 
 const RemedyResultsContainer = (props) => {
+    // console.log("props for RemedyResultsContainer:", props);
+    const { treatment } = props;
+    let numberOfResults = treatment.length;
+    let remedyName = treatment.map((treatmentInfo, index) => {
+        return <RemedyNameContainer key={index} remedyName={treatmentInfo.Herb} />
+    })
     return (
         <div className="remedy-results-container">
             <div className="remedy-result-total-message">
                 <span>Remedy Results:
-                        <span className="remedy-result-total-number">7</span>
+                        <span className="remedy-result-total-number"> {numberOfResults}</span>
                 </span>
             </div>
             <div className="remedy-results-group">
                 <div className="remedy-results">
-                    <RemedyNameContainer />
+                    {remedyName}
                     <EndingMessage />
                 </div>
             </div>
