7d0ac6326de3d855798dc0c9cab8f4d7e3221258
Mia Tran <maiptran157@gmail.com>
2018-10-11 21:04:27 -0700

    wip. add review form

--- client/src/components/remedy_detail_container/index.js
+++ client/src/components/remedy_detail_container/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './remedy_detail_container.css';
 import RemedyDetailGroup from './remedy_detail_group';
-import OtherTreamentsContainer from './other_treatments_container';
+import RemedyReviews from './remedy_reviews';
 import Header from '../header';
 import dummyData from '../../dummy_data/data_for_remedy_detail';
 import backButton from '../../assets/images/back_arrow_white_shadow.png';
@@ -23,7 +23,7 @@ const RemedyDetailContainer = (props) => {
         <div className="condition-detail-container">
             <Header logo={backButton} buttonType="back-button" />
             <RemedyDetailGroup name={Herb} remedyDesc={Remedy} note={Note} caution={Caution} />
-            <OtherTreamentsContainer _id={remedyId} />
+            <RemedyReviews _id={remedyId} />
         </div>
     )
 }
