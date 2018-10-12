5de7dc881e80902c934d85e9c56e3976f278881e
Jonathan Gallo <jonnygallo@gmail.com>
2018-10-12 11:55:48 -0700

    changed header to wooden styling, updated logo and fixed hamburger menu icon positioning

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Logo from './logo';
 import InputBar from './input_bar';
 import DropDownMenu from '../menu';
+import woodenHeader from '../../assets/images/wooden_header1080x120.png';
 
 import './header.css';
+
+const backgroundHeader = { backgroundImage: `url(${ woodenHeader })` }
 
 export default props => {
     const { logo, buttonType } = props;
     return (
-        <div className="header">
+        <div className="header" style={ backgroundHeader }>
             <Logo logo={logo} buttonType={buttonType} />
             <InputBar />
             <DropDownMenu />
