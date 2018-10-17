f9324c91ee2d810d84cf518f681b066ac5d264b2
Perry Tran <ptran209@gmail.com>
2018-10-16 17:27:30 -0700

    fixed css and styling for modal

--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import './modal.css';
 
-class Modal extends Component {
-    render() {
-        console.log(this.props);
-        if (this.props.isOpen === false) {
+class AgreementModal extends Component {
+    state = {
+        isOpen: true
+    };
+
+    open = () => this.setState({isOpen: true});
+
+    close = () => this.setState({isOpen: false});
+
+    render(){
+
+        if(this.state.isOpen){
+            return (
+                <div className="basic-modal" onClick={this.close}>
+                    <div onClick={e => e.stopPropagation()} className="basic-modal-content">
+                        {/* <div onClick={this.close} className="basic-modal-close">X</div> */}
+                        <h1>Disclaimer</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis fugiat laboriosam quo? Ab officia tempore ratione id, modi possimus adipisci autem, inventore reiciendis facere nesciunt.</p>
+                        <div className="agree-button">
+                            <button onClick={this.close}>Agree</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            )
+        } else {
             return null;
         }
-        return (   
-            <div>
-                <div className="modal">{this.props.children}</div>
-                <div className="backdrop"></div>
-            </div>
-            
-        )
     }
 }
 
-export default Modal;
+export default AgreementModal;
