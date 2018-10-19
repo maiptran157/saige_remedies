c377da282f931cc223f419f82cdd464615c89d23
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-10-18 17:52:04 -0700

    added working check for modal and session

--- client/public/dist/app.php
+++ client/public/dist/app.php
@@ -8,20 +8,38 @@ require_once('mysql_connect.php');
 $request = $_GET['request'];
 
 switch($request){
-    case 'symptom_category':
-        require_once('symptom_category.php');
+    case 'check_login':
+    require_once('check_login.php');
+    break;
+    case 'login':
+        require_once('login.php');
+        break;
+    case 'logout':
+        require_once('logout.php');
+        break;
+    case 'modal_agree':
+        require_once('modal_agree.php');
+        break;
+    case 'modal_check':
+        require_once('modal_check.php');
         break;
     case 'remedy_detail':
         require_once('remedy_detail.php');
         break;
-    case 'reviews':
-        require_once('reviews.php');
+    case 'search_symptom':
+        require_once('search_symptom.php');
         break;
-    case 'login':
-        require_once('login.php');
+    case 'session_check':
+        require_once('session_check.php');
         break;
     case 'sign_up': 
         require_once('sign_up.php');
+        break;
+    case 'submit_review':
+        require_once('submit_review.php');
+        break;
+    case 'symptom_category':
+        require_once('symptom_category.php');
         break;
     case 'symptom_details': 
         require_once('symptom_details.php');
@@ -29,14 +47,8 @@ switch($request){
     case 'symptom_list':
         require_once('symptom_list.php');
         break;
-    case 'search_symptom':
-        require_once('search_symptom.php');
-        break;
     case 'user_sign_up_check':
         require_once('user_sign_up_check.php');
-        break;
-    case 'submit_review':
-        require_once('submit_review.php');
         break;
     default:
         $output['Errors'] = 'Request URL failed';
