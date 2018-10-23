45bf6763ce0eb95e2e5aded643d815d7a19acb99
Perry Tran <ptran209@gmail.com>
2018-10-23 14:42:36 -0700

    stopped modal from loading when users are logged in

--- client/src/components/umbrella_conditions/index.js
+++ client/src/components/umbrella_conditions/index.js
@@ -3,21 +3,26 @@ import Header from '../header';
 import { connect } from 'react-redux'
 import { getCategoryList } from '../../actions/index'
 import CategoryContainer from '../category_container';
+import AgreementModal from '../modal';
 import './umbrella_conditions.scss';
 import saigeLogo from '../../assets/images/saige_logo_no_stem_100px.png';
 
 class UmbrellaConditions extends Component {
+    state = {
+        modalCheck: localStorage.getItem('loggedin'),
+    }
+
     componentDidMount() {
         this.props.getCategoryList();
     }
 
     render() {
+        const { modalCheck } = this.state;
         const { categories, history } = this.props;
         const categoryList = () => {
             if (!categories) {
                 return null;
             } else {
-
                 return categories.map((category) => {
                     return <CategoryContainer id={category._id} img={category.img} key={category._id} name={category.name} />
                 });
@@ -26,6 +31,7 @@ class UmbrellaConditions extends Component {
         }
         return (
             <div className="categories-container">
+                {modalCheck ? null : <AgreementModal/>  }
                 <Header push={history.push} logo={saigeLogo} />
                 <div className="categories-content">
                     {categoryList()}
