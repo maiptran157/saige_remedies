ddaa5665af863ffb3cf9c43365c4e8dacc10dc1e
Mia Tran <maiptran157@gmail.com>
2018-11-29 16:00:30 -0800

    install Popup component and complete popup explanation for remedy techniques

--- client/src/components/remedy_detail_container/remedy_detail_group.js
+++ client/src/components/remedy_detail_container/remedy_detail_group.js
@@ -1,76 +1,125 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './remedy_detail_container.css';
+import Popup from 'reactjs-popup';
+import techniques from './techniques';
 
 const style = {
-    fontWeight: "bold"
+    fontWeight: 'bold'
 }
 
 const liStyle = {
     listStyleType: 'none'
 }
 
-const RemedyDetailGroup = (props) => {
-    const { name, remedyDesc, note, caution } = props;
-    const displayRemedyDescSteps = () => {
-        const remedyDescSteps = remedyDesc.split(". ");
-        if (remedyDescSteps.length === 1) {
-            return remedyDesc;
+class RemedyDetailGroup extends Component {
+    formatSteps = (label, value, description) => {
+        if (!value) {
+            return label;
         }
-        let step = 1;
-        for (let i = 0; i < remedyDescSteps.length; i++) {
-            remedyDescSteps[i] = `${step}. ${remedyDescSteps[i]}`
-            step++
+        return (<span>
+            {label.split(value)
+                .reduce((prev, current, i) => {
+                    if (!i) {
+                        return [current];
+                    }
+                    return prev.concat(<Popup key={i} trigger={<span className="technique">{value}</span>} position="right center">
+                        <div>{description}</div>
+                    </Popup>, current);
+                }, [])
+            }
+        </span>);
+    };
+
+    render() {
+        const { name, remedyDesc, note, caution } = this.props;
+        const displayRemedyDescSteps = () => {
+            const remedyDescSteps = remedyDesc.split(". ");
+            for (let i = 0; i < remedyDescSteps.length; i++) {
+                // for (let key in techniques) {
+                //     if (remedyDescSteps[i].includes(key)) {
+                //         remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], key, techniques[key])
+                //     }
+                // }
+                if (remedyDescSteps[i].includes('infusion')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'infusion', techniques['infusion'])
+                } else if (remedyDescSteps[i].includes('decoction')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'decoction', techniques['decoction'])
+                } else if (remedyDescSteps[i].includes('tincture')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'tincture', techniques['tincture'])
+                } else if (remedyDescSteps[i].includes('powder')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'powder', techniques['powder'])
+                } else if (remedyDescSteps[i].includes('capsule')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'capsule', techniques['capsule'])
+                } else if (remedyDescSteps[i].includes('syrup')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'syrup', techniques['syrup'])
+                } else if (remedyDescSteps[i].includes('infused oil')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'infused oil', techniques['infused oil'])
+                } else if (remedyDescSteps[i].includes('essential oil')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'essential oil', techniques['essential oil'])
+                } else if (remedyDescSteps[i].includes('ointment')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'ointment', techniques['ointment'])
+                } else if (remedyDescSteps[i].includes('cream')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'cream', techniques['cream'])
+                } else if (remedyDescSteps[i].includes('lotion')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'lotion', techniques['lotion'])
+                } else if (remedyDescSteps[i].includes('poultice')) {
+                    remedyDescSteps[i] = this.formatSteps(remedyDescSteps[i], 'poultice', techniques['poultice'])
+                }
+            }
+            if (remedyDescSteps.length === 1) {
+                return remedyDescSteps[0];
+            }
+            return remedyDescSteps.map((remedy, index) => {
+                return <li style={liStyle} key={index}> {index + 1}. {remedy}</li>
+            })
         }
-        return remedyDescSteps.map((remedy, index) => {
-            return <li style={liStyle} key={index}>{remedy}</li>
-        })
-    }
-    const displayRemedyDesc = () => {
-        if (note === undefined && caution === undefined) {
-            return (
-                <p className="detail-paragraph">
-                    {displayRemedyDescSteps()}
-                </p>
-            )
-        } else if (note === undefined) {
-            return (
-                <p className="detail-paragraph">
-                    {displayRemedyDescSteps()}
-                    <br />
-                    <span style={style}>Caution:</span>{caution}
-                </p>
-            )
-        } else if (caution === undefined) {
-            return (
-                <p className="detail-paragraph">
-                    {displayRemedyDescSteps()}
-                    <br />
-                    <span style={style}>Note:</span>{note}
-                </p>
-            )
-        } else {
-            return (
-                <p className="detail-paragraph">
-                    {displayRemedyDescSteps()}
-                    <br />
-                    <span style={style}>Note:</span>{note}
-                    <br />
-                    <span style={style}>Caution:</span>{caution}
-                </p>
-            )
+        const displayRemedyDesc = () => {
+            if (note === undefined && caution === undefined) {
+                return (
+                    <ul className="detail-paragraph">
+                        {displayRemedyDescSteps()}
+                    </ul>
+                )
+            } else if (note === undefined) {
+                return (
+                    <ul className="detail-paragraph">
+                        {displayRemedyDescSteps()}
+                        <br />
+                        <span style={style}>Caution:</span>{caution}
+                    </ul>
+                )
+            } else if (caution === undefined) {
+                return (
+                    <ul className="detail-paragraph">
+                        {displayRemedyDescSteps()}
+                        <br />
+                        <span style={style}>Note:</span>{note}
+                    </ul>
+                )
+            } else {
+                return (
+                    <ul className="detail-paragraph">
+                        {displayRemedyDescSteps()}
+                        <br />
+                        <span style={style}>Note:</span>{note}
+                        <br />
+                        <span style={style}>Caution:</span>{caution}
+                    </ul>
+                )
+            }
         }
-    }
-    const fullDesc = displayRemedyDesc();
-    return (
-        <div className="remedy-detail-group">
-            <div className="remedy-detail-header">
-                {name}
+        const fullDesc = displayRemedyDesc();
+        return (
+            <div className="remedy-detail-group">
+                <div className="remedy-detail-header">
+                    {name}
+                </div>
+                <div className="remedy-detail-description">
+                    {fullDesc}
+                </div>
             </div>
-            <div className="remedy-detail-description">
-                {fullDesc}
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RemedyDetailGroup;
---
+++ client/src/components/remedy_detail_container/techniques.js
@@ -0,0 +1,14 @@
+export default {
+    'infusion': "a drink, remedy, or extract prepared by soaking the leaves of a plant or herb in liquid",
+    'decoction': "the liquor resulting from concentrating the essence of a substance by heating or boiling, especially a medicinal preparation made from a plant",
+    'tincture': "a medicine made by dissolving a drug in alcohol",
+    'capsule': "a capsule is a very small tube containing powdered or liquid medicine, which you swallow",
+    'powder': "fine dry particles produced by the grinding, crushing, or disintegration of a solid substance",
+    'syrup': "a thick sweet liquid containing medicine or used as a drink",
+    'infused oil': "carrier oils that have been used as a solvent to extract the therapeutic properties of a certain plant or plants",
+    'essential oil': "any of a class of volatile oils that give plants their characteristic odors and are used especially in perfumes and flavorings, and for aromatherapy",
+    'ointment': "a smooth oily preparation that is rubbed on the skin for medicinal purposes or as a cosmetic",
+    'cream': "a water-soluble medicinal preparation applied to the skin",
+    'lotion': "a liquid preparation for cosmetic or external medicinal use",
+    'poultice': "a soft, moist mass of material, typically of plant material or flour, applied to the body to relieve soreness and inflammation and kept in place with a cloth",
+}
