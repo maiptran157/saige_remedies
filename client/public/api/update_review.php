83abe18f23e202ac90eb847124f7acc1eca60c68
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-01-10 17:16:46 -0800

    updated review update endpoint

--- client/public/api/update_review.php
+++ client/public/api/update_review.php
@@ -6,26 +6,30 @@ $review = $_POST['review'];
 $rating = $_POST['rating'];
 $reviewID = $_POST['reviewID'];
 $action = $_POST['action'];
+$execute = false;
 
 $output = [];
 
 if($action === 'remove'){
     $stmtReview = $conn->prepare("DELETE FROM reviews WHERE ID = ?");
     $stmtReview->bind_param("s", $reviewID);
-    $stmtReview->execute();
-    $stmtReview->close();
     $output['message'] = 'Review has been removed';
+    $execute = true;
 } else{
     if($action === 'update' && $review && $rating){
         $stmtReview = $conn->prepare("UPDATE reviews SET rating = ?, review= ? WHERE ID = ?");
         $stmtReview->bind_param("sss", $rating, $review, $reviewID);
-        $stmtReview->execute();
-        $stmtReview->close();
         $output['message'] = 'Review has been updated';
+        $execute = true;
     } else{
         $output['message'] = 'Please include both a review and rating';
     }}
 
-
+if($execute){
+    $stmtReview->execute();
+    $stmtReview->close();
+} else {
+    $output['message'] = 'Changes to review failed';
+}
 
 ?>
